c159eb984971702a1602c13eb766264f4f4b82ea
Holden Altaffer <haltaffer@gmail.com>
2018-02-21 06:10:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speak)
-if(speak == 'I LOVE YOU GRANDMA!')
+if speak == 'I LOVE YOU GRANDMA!'
   'I LOVE YOU TOO PUMPKIN!'
-elsif(speak == speak.upcase)
+elsif speak == speak.upcase
     'NO, NOT SINCE 1938!'
 else
   'HUH?! SPEAK UP, SONNY!'
