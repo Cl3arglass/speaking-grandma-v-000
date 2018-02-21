ce9b7ae5e216374c2224b371072a5ec28a4b47a6
Holden Altaffer <haltaffer@gmail.com>
2018-02-21 06:09:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(speak)
 if(speak == 'I LOVE YOU GRANDMA!')
   'I LOVE YOU TOO PUMPKIN!'
 elsif(speak == speak.upcase)
-    NO, NOT SINCE 1938!
+    'NO, NOT SINCE 1938!'
 else
-  HUH?! SPEAK UP, SONNY!
+  'HUH?! SPEAK UP, SONNY!'
 end
